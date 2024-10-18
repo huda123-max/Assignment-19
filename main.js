550826420479f2e09010f0fb21f4d1d12aa93294
huda123-max <hm7315150@gmail.com>
2024-10-17 23:07:34 -0700

    Assignment 19

---
+++ main.js
@@ -0,0 +1,6 @@
+//Assignment 19
+var Guest_list = ['Huda', 'Tayaba', 'Irshad', 'Sara', 'Mariyam'];
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Respected Madam/Sir ".concat(Guest_list[i], ", \n We are inviting you for the dinner party"));
+}
+console.log("Total number of Guests are: ".concat(Guest_list.length));
---
+++ main.ts
@@ -0,0 +1,9 @@
+//Assignment 19
+
+let Guest_list:string[]=['Huda','Tayaba','Irshad','Sara','Mariyam'];
+
+for(let i=0; i<Guest_list.length; i++){
+    console.log(`Respected Madam/Sir ${Guest_list[i]}, \n We are inviting you for the dinner party`);
+}
+
+console.log(`Total number of Guests are: ${Guest_list.length}`);
